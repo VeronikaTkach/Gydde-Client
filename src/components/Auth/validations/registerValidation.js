cf69fbe77c9f43ffb873cd9fa599abdf90665b89
Elvira <gaellru@gmail.com>
2023-12-23 21:01:58 +0300

    created validates folder
    created validates folder and moved regexps and validation objects there from register component

---
+++ src/components/Auth/validations/registerValidation.js
@@ -0,0 +1,40 @@
+export const mailRegExp =
+  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+export const nicknameRegExp = /^[A-Za-z0-9_\-.@]+$/;
+export const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s])/;
+
+export const mailValidation = {
+  required: 'Required!',
+  pattern: {
+    value: mailRegExp,
+    message: 'Enter a valid email address',
+  },
+  minLength: {
+    value: 6,
+    message: 'Email must be at least 6 characters long',
+  },
+};
+
+export const nicknameValidation = {
+  required: 'Required!',
+  pattern: {
+    value: nicknameRegExp,
+  },
+  maxLength: 20,
+};
+
+export const passwordValidation = {
+  required: 'Required!',
+  pattern: {
+    value: passwordRegExp,
+    message: 'The password must contain Latin characters, special characters, numbers',
+  },
+  minLength: {
+    value: 8,
+    message: 'Password must be at least 8 characters long',
+  },
+  maxLength: {
+    value: 30,
+    message: 'Password must be no more than 30 characters',
+  },
+};
