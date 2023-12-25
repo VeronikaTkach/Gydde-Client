d3d338dc0c2c11a50afb018b5815349f07eeeb08
Elvira <gaellru@gmail.com>
2023-12-26 02:24:23 +0300

    update extraReducers with actual syntax

--- src/core/store/slices/registrationSlice.js
+++ src/core/store/slices/registrationSlice.js
@@ -4,36 +4,40 @@ import { Status } from '../../constants/Status';
 
 export const registerUser = createAsyncThunk(
   'registration/registerUser',
-  async function(regData, { rejectWithValue }) {
+  async function (regData, { rejectWithValue }) {
     try {
       const response = await axios.post('http://localhost:3004/register', regData);
+      localStorage.setItem('accessToken', response.data.accessToken);
 
       return response.data;
     } catch (error) {
-      return rejectWithValue(error);
+      return rejectWithValue(error.message);
     }
   }
 );
 
-const initialState = {};
+const initialState = {
+  status: null,
+  error: null,
+};
 
 export const registrationSlice = createSlice({
   name: 'registration',
   initialState,
   reducers: {},
-  extraReducers: {
-    [registerUser.pending]: (state) => {
-      state.status = Status.Loading;
-      state.error = null;
-    },
-    [registerUser.fulfilled]: (state, action) => {
-      state.status = Status.Resolved;
-      state.todos = action.payload;
-    },
-    [registerUser.rejected]: (state, action) => {
-      state.status = Status.Rejected;
-      state.error = action.payload.message;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.status = Status.Loading;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.status = Status.Resolved;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.status = Status.Rejected;
+        state.error = action.payload;
+      });
   },
 });
 
