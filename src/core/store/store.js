689e4532b79bf5d77deb9714f69ca572abcdc677
Elvira <gaellru@gmail.com>
2023-12-26 02:24:47 +0300

    add registrationSlice

--- src/core/store/store.js
+++ src/core/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import statePopupWindow from './slices/windowStateSlice';
+import registrationSlice from './slices/registrationSlice';
 
 export const store = configureStore({
   reducer: {
     statePopupWindow,
+    registration: registrationSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
 });
