fbb78d6f487699a642fa79eb62e34231e6d2e2e0
tovchenyk <vladyslav.tovchenyk@gmail.com>
2023-12-25 17:33:35 +0100

    Fix google oauth authentication.

--- src/core/constants/authKeys.js
+++ src/core/constants/authKeys.js
@@ -1,6 +1,6 @@
 export const GOOGLE_CLIENT_ID =
-  '244335238910-4ov3btm2t2t0hlfoe23utpqd96thp8gv.apps.googleusercontent.com';
-export const GOOGLE_SECRET_KEY = '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';
+  '551960957643-53os0gglonuj5fn27jb9lk8d27a57u1b.apps.googleusercontent.com';
+export const GOOGLE_SECRET_KEY = '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';
 
 export const TWITTER_CLIENT_ID = '7CFE03Qo4wMOYyzkHJspYpDG1';
 export const TWITTER_SECRET_KEY = 'QJE6nknjnP2smI0iMv3hCHrFuk9DTzOb8GNMVixDPanSDiBELG';
