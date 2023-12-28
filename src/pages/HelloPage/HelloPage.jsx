1304720df7d5a9936d0acbc3a8ffa7f754c39436
gleg0r <egor103230@gmail.com>
2023-12-28 10:51:12 +0300

    add: change languages with interval 3 second while user wont change lang

--- src/pages/HelloPage/HelloPage.jsx
+++ src/pages/HelloPage/HelloPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from '../../components/ui/buttons/Button/Button';
 import cn from 'classnames';
 import s from './style.module.scss';
@@ -21,13 +21,34 @@ const languages = [
 ];
 
 export function HelloPage() {
-
-  //const locale = useLocale(); 
-
   const [clickedElement, setClickedElement] = useState('English');
+  const [stop, setStop] = useState(false);
+  const [intervalLang, setIntervalLang] = useState(null);
   function handleClick(element) {
     setClickedElement(element);
+    setStop(true);
   }
+
+  useEffect(() => {
+    if (stop) {
+      clearInterval(intervalLang);
+    }
+  }, [stop, intervalLang]);
+  let i = 1;
+  useEffect(() => {
+    const interval = setInterval(updateLang, 3000);
+
+    function updateLang() {
+      setClickedElement(languages[i].lang);
+      i++;
+      if (i === languages.length) {
+        i = 0;
+      }
+    }
+    setIntervalLang(interval);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <main className={cn(s.content)}>
@@ -59,22 +80,19 @@ export function HelloPage() {
       </div>
       <div className={cn(s.content__subtitles)}>
         <div className={cn(s.subtitles__text)}>
-          <Button
-            className={cn(s.subtitles__speaker)}
-          >
-            <img src={speaker} alt="speaker" />
+          <Button className={cn(s.subtitles__speaker)}>
+            <img src={speaker} alt='speaker' />
           </Button>
-          <h2 className={cn(s.subtitles__greeting)}>Hello! I’m <span>Gydde!</span></h2>
-          
+          <h2 className={cn(s.subtitles__greeting)}>
+            Hello! I’m <span>Gydde!</span>
+          </h2>
         </div>
         <div className={cn(s.subtitles__action)}>
-            <Button
-              className={cn(s.subtitles__answer)}s
-            >
-              Hi, Gydde!
-              <img src={hand} alt='hand' />
-            </Button>
-            <img className={cn(s.subtitles__arrow)} src={arrow} alt="arrow" />
+          <Button className={cn(s.subtitles__answer)}>
+            Hi, Gydde!
+            <img src={hand} alt='hand' />
+          </Button>
+          <img className={cn(s.subtitles__arrow)} src={arrow} alt='arrow' />
         </div>
       </div>
     </main>
