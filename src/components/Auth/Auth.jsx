b0a3da016189c2e9024a6a925956bd077743e802
Elvira <gaellru@gmail.com>
2023-12-28 22:43:18 +0300

    update auth

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,46 +1,48 @@
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import cn from 'classnames';
+import IconE from '../../assets/images/email.svg';
 import IconM from '../../assets/images/metamask.svg';
-import IconT from '../../assets/images/trust.png';
-import IconB from '../../assets/images/binance.svg';
 import { Button } from '../ui/buttons/Button';
 import { Metamask } from './Metamask';
 import s from './style.module.scss';
+import { RoutesName } from '../../core/constants/Routes';
+import { Google } from './Google';
+import { Twitter } from './Twitter';
 
 export function Auth() {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   return (
     <div className={s.auth}>
-      <div className={s.auth__title}>Log in to Gydde</div>
-      <div className={cn(s.auth__social)}>
-        <div className={cn(s.social__title)}>Log in with social account</div>
-        <div className={cn(s.social__body)}>
-          <Link className={cn(s.social, s.social_twitter)} to='#'>
-            <img className={s.social__img} src={IconM} />
-            <div className={s.social__text}>Twitter</div>
-          </Link>
-          <Link className={cn(s.social, s.social_trust)} to='#'>
-            <img className={s.social__img} src={IconT} />
-            <div className={s.social__text}>Google</div>
-          </Link>
-          <Link className={cn(s.social, s.social_binance)} to='#'>
-            <img className={s.social__img} src={IconB} />
-            <div className={s.social__text}>Email</div>
-          </Link>
-        </div>
+      <div className={s.auth__header}>
+        <div className={s.auth__title}>Log in to Gydde</div>
+        <Button
+          className={s.auth__close}
+          onClick={() => navigate(RoutesName.Back)}></Button>
       </div>
-      <div className={cn(s.auth__social)}>
-        <div className={cn(s.social__title)}>Log in with wallet</div>
-        <div className={cn(s.social__body)}>
-          <Link className={cn(s.social, s.social_metamask)} to='#'>
-            <img className={s.social__img} src={IconM} />
-            <div className={s.social__text}>
-              <Metamask />
-              Metamask
-            </div>
-          </Link>
+      <div className={s.auth__body}>
+        <div className={cn(s.auth__socials, s.socials)}>
+          <div className={cn(s.socials__title)}>Log in with social account</div>
+          <div className={cn(s.socials__body)}>
+            <Twitter className={cn(s.social, s.social_twitter)} />
+            <Google className={cn(s.social, s.social_google)} />
+            <Link className={cn(s.social, s.social_email)} to={RoutesName.MailAuthPage}>
+              <img className={s.social__img} src={IconE} />
+              <div className={s.social__text}>Email</div>
+            </Link>
+          </div>
+        </div>
+        <div className={cn(s.auth__socials, s.socials)}>
+          <div className={cn(s.socials__title)}>Log in with wallet</div>
+          <div className={cn(s.socials__body)}>
+            <Link className={cn(s.social, s.social_metamask)} to='#'>
+              <img className={s.social__img} src={IconM} />
+              <div className={s.social__text}>
+                <Metamask />
+                Metamask
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
