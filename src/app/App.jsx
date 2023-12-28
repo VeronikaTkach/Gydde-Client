5a49bd4dbd0aa1ffc2dd2edffa9a72335ca317dc
Elvira <gaellru@gmail.com>
2023-12-28 17:17:53 +0300

    add routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,7 +3,10 @@ import { HomePage } from '../pages/HomePage';
 import { LayoutMain } from '../layouts/LayoutMain';
 import './style.scss';
 import { OauthPage } from '../pages/OauthPage/OauthPage';
-import { HelloPage } from "../pages/HelloPage/HelloPage";
+import { HelloPage } from '../pages/HelloPage/HelloPage';
+import { AuthPage } from '../pages/auth/AuthPage';
+import { MailAuthPage } from '../pages/auth/MailAuthPage';
+import { RoutesName } from '../core/constants/Routes';
 
 export function App() {
   return (
@@ -11,6 +14,8 @@ export function App() {
       <Route path='/' element={<LayoutMain />}>
         <Route path='/dd' element={<HomePage />} />
         <Route path='/oauth' element={<OauthPage />} />
+        <Route path={RoutesName.AuthPage} element={<AuthPage />} />
+        <Route path={RoutesName.MailAuthPage} element={<MailAuthPage />} />
         <Route path='/hello' element={<HelloPage />} />
       </Route>
     </Routes>
