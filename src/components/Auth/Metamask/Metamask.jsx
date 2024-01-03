5d6f143bdf62432419874e1ffd2f99fd3500b806
Natali <nattalli.gf@gmail.com>
2024-01-03 21:05:24 +0200

    add: icons font, web3 library

--- src/components/Auth/Metamask/Metamask.jsx
+++ src/components/Auth/Metamask/Metamask.jsx
@@ -1,12 +1,16 @@
 import { useDispatch } from 'react-redux';
 import cn from 'classnames';
+import Web3 from 'web3';
 import { showAuthorizationWindow } from '../../../core/store/slices/windowStateSlice';
 import { Button } from '../../ui/buttons/Button';
 import s from './style.module.scss';
 
-export function Metamask({ className, isConnected }) {
+// const web3 = new Web3(Web3.givenProvider);
+
+export function Metamask({ className }) {
   const dispatch = useDispatch();
 
+  //обсудить путь с беком, не совсем ясно что по итогу ему нужно
   async function onConnect() {
     if (window.ethereum) {
       window.ethereum
@@ -21,9 +25,22 @@ export function Metamask({ className, isConnected }) {
     }
   }
 
+  async function requestEthereumAddress() {
+    const web3 = new Web3(window.ethereum);
+    try {
+      const accounts = await web3.eth.requestAccounts();
+
+      const code = Math.floor(Math.random() * 1000000).toString();
+
+      const signature = await web3.eth.personal.sign(code, accounts[0], '');
+
+      console.log(accounts, code, signature);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <Button className={cn(s.button, className)} onClick={onConnect}>
-      {isConnected && <div className={s.connected}></div>}
-    </Button>
+    <Button className={cn(s.button, className)} onClick={requestEthereumAddress}></Button>
   );
 }
