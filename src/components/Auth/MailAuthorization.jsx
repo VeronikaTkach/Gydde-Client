59518bc3c888b22f5c54f6be14d70ff24d11ba6b
Veronika <xafling@mail.ru>
2024-01-08 13:52:58 +0200

    add icons close and arrow

--- src/components/Auth/MailAuthorization.jsx
+++ src/components/Auth/MailAuthorization.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { FormAuthorization } from './forms';
 import { Button } from '../ui/buttons/Button';
 import s from './style.module.scss';
+import cn from 'classnames';
 import { RoutesName } from '../../core/constants/Routes';
 
 export function MailAuthorization() {
@@ -14,12 +15,12 @@ export function MailAuthorization() {
         <div className={s.auth__header}>
           <div className={s.auth__header_row}> 
             <Button
-            className={s.auth__back}
+            className={cn(s.auth__back, 'iconArrowBack')}
             onClick={() => navigate(RoutesName.Back)}></Button>
             <div className={s.auth__title}>Log in to Gydde</div>
           </div>
           <Button
-            className={s.auth__close}
+            className={cn(s.auth__close, 'iconClose')}
             onClick={() => navigate(RoutesName.Root)}></Button>
         </div>
         <FormAuthorization />
