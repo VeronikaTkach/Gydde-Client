10aadf644d4ea9f3ae50bbb3b5fca31616d8ff2a
gleg0r <egor103230@gmail.com>
2024-01-08 19:33:39 +0300

    fix: background on hello page

--- src/layouts/LayoutMain/LayoutMain.jsx
+++ src/layouts/LayoutMain/LayoutMain.jsx
@@ -1,5 +1,7 @@
-import { Outlet } from 'react-router-dom';
+import { RoutesName } from '../../core/constants/Routes';
+import { Outlet, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import cn from 'classnames';
 import { Header } from '../../components/mainParts/Header';
 import { Auth } from '../../components/Auth';
 import s from './style.module.scss';
@@ -7,8 +9,11 @@ import s from './style.module.scss';
 export function LayoutMain() {
   const { authorizationWindowState } = useSelector((state) => state.statePopupWindow);
 
+  const path = useLocation().pathname;
+
   return (
-    <div className={s.wrapper}>
+    <div
+      className={cn(s.wrapper, { [s.wrapper__lights]: path === RoutesName.HelloPage })}>
       <div className={s.wrapper__container}>
         {authorizationWindowState && <Auth className={s.wrapper__authorization} />}
         <Header />
