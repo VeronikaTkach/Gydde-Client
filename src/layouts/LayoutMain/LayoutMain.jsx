ad4c7bcc453b7ccdbe65e68c3c2eee731ba6f7d7
Natali <nattalli.gf@gmail.com>
2024-01-15 10:56:41 +0300

    added: scss resize

--- src/layouts/LayoutMain/LayoutMain.jsx
+++ src/layouts/LayoutMain/LayoutMain.jsx
@@ -3,14 +3,14 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { RoutesName } from '../../core/constants/Routes';
 import { Header } from '../../components/mainParts/Header';
 import s from './style.module.scss';
-import { useResize } from '../../core/hooks/resize';
+// import { useResize } from '../../core/hooks/resize';
 
-const staticHeight = 1080;
-const staticWidth = 1600;
+// const staticHeight = 1080;
+// const staticWidth = 1600;
 
 export function LayoutMain() {
   const path = useLocation().pathname;
-  const { height, zoomValue, maxAvailableWidth } = useResize(staticWidth, staticHeight);
+  // const { height, zoomValue, maxAvailableWidth } = useResize(staticWidth, staticHeight);
 
   return (
     <div
@@ -19,11 +19,13 @@ export function LayoutMain() {
       })}>
       <div
         className={s.wrapper__container}
-        style={{
-          transform: `scale(${zoomValue})`,
-          maxHeight: `${height}px`,
-          minWidth: `${maxAvailableWidth}px`,
-        }}>
+        style={
+          {
+            // transform: `scale(${zoomValue})`,
+            // maxHeight: `${height}px`,
+            // minWidth: `${maxAvailableWidth}px`,
+          }
+        }>
         <Header />
         <Outlet />
       </div>
