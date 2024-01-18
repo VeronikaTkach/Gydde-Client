de2e0032d77173f03eeb7a152fc509c58bb1bdf0
gleg0r <egor103230@gmail.com>
2024-01-19 00:13:19 +0300

    add: quest cards

--- src/components/quests/QuestChatCard/QuestChatCard.js
+++ src/components/quests/QuestChatCard/QuestChatCard.js
@@ -1,18 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 import s from './style.module.scss';
-import { Button } from '../ui/buttons/Button';
 
+const zeroNotifications = 0;
 
-export function QuestChatCard({ className }) {
-
-  useEffect(() => {
-
-  }, []);
-
+export function QuestChatCard({ className, text, img, isActive }) {
   return (
-    <div className={cn(s.questCard, className)}>
-      
+    <div className={cn(isActive === false ? s.questCard : s.questCard_active, className)}>
+      <img className={s.questCard__img} src={img} alt='image of guide' />
+      <div className={cn(s.questCard__info)}>
+        <div className={s.questCard__text}>
+          <h4 className={s.questCard__title}>{text.title}</h4>
+          <p className={s.questCard__author}>{text.author}</p>
+        </div>
+        <div className={s.questCard__desc}>
+          {text.notification !== zeroNotifications ? (
+            <p className={s.questCard__notification}>{text.notification}</p>
+          ) : (
+            <div className={s.questCard__notification_blank}></div>
+          )}
+          <p
+            className={cn(
+              text.progress === 'Completed'
+                ? s.questCard__progress_ready
+                : s.questCard__progress
+            )}>
+            {text.progress}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/quests/QuestSidePannel/QuestSidePannel.js
+++ src/components/quests/QuestSidePannel/QuestSidePannel.js
@@ -3,17 +3,68 @@ import cn from 'classnames';
 import s from './style.module.scss';
 import { Button } from '../../ui/buttons/Button';
 import { QuestSearch } from '../QuestSearch';
+import { QuestChatCard } from '../QuestChatCard';
+import questImage from '../../../assets/images/questImage.png';
 
+const card = [
+  {
+    title: 'The Great Battle',
+    author: 'PancakeSwaper',
+    progress: 'In progress',
+    notification: 0,
+    id: 0,
+  },
+  {
+    title: 'The Great Battle',
+    author: 'PancakeSwaper',
+    progress: 'In progress',
+    notification: 1,
+    id: 1,
+  },
+  {
+    title: 'The Great Battle',
+    author: 'PancakeSwaper',
+    progress: 'Completed',
+    notification: 0,
+    id: 2,
+  },
+  {
+    title: 'The Great Battle',
+    author: 'PancakeSwaper',
+    progress: 'Completed',
+    notification: 1,
+    id: 3,
+  },
+];
+
+const firstItem = 0;
 
 export function QuestSidePannel({ className }) {
-
+  const [activeQuest, setActiveQuest] = useState(firstItem);
   useEffect(() => {
 
   }, []);
 
+  function handleClick(index) {
+    setActiveQuest(index);
+  }
+
   return (
     <div className={cn(s.questSidePannel, className)}>
       <QuestSearch />
+      <ul>
+        {card.map((item, index) => {
+          return (
+            <li key={item.id} onClick={() => handleClick(index)}>
+              <QuestChatCard
+                isActive={activeQuest === index}
+                text={item}
+                img={questImage}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
