e48e0431cd1bd00bc1f08103df71c0e9e9540927
Elvira <gaellru@gmail.com>
2024-01-19 01:34:06 +0300

    add Howl and play/pause function

--- src/components/soundControls/AudioPlayer/AudioPlayerWithProgressBar.jsx
+++ src/components/soundControls/AudioPlayer/AudioPlayerWithProgressBar.jsx
@@ -1,48 +1,96 @@
 import { useEffect, useRef, useState } from 'react';
-import sudio from '../../../assets/audio/olivia-russian.mp3';
+import { Howl } from 'howler';
+import cn from 'classnames';
+import audio from '../../../assets/audio/olivia-russian.mp3';
+import { PlayButton } from '../PlayButton/PlayButton';
+import { SoundSwitchStatus } from '../../../core/constants/SoundSwitchStatus';
+import s from './style.module.scss';
+
+const sound = new Howl({
+  src: audio,
+  html5: true,
+});
 
 export const AudioPlayerWithProgressBar = () => {
-  const audioRef = useRef(null);
-  const [progress, setProgress] = useState(0);
-  const [duration, setDuration] = useState(0);
+  // let status = SoundSwitchStatus.On;
+  const [switchStatus, setSwitchStatus] = useState(SoundSwitchStatus.On);
+  // const sound = new Howl({
+  //   src: audio,
+  //   html5: true,
+  // });
+  const soundPlay = () => {
+    // setSwitchStatus(status);
+    if (switchStatus) {
+      sound.play();
+      // status = SoundSwitchStatus.Off;
+      setSwitchStatus(SoundSwitchStatus.Off);
 
-  const updateTime = () => {
-    const current = audioRef.current.currentTime;
-    const total = audioRef.current.duration;
-    setProgress((current / total) * 100);
+      // console.log('pause', status);
+
+      // return status;
+    } else {
+      sound.pause();
+      // status = SoundSwitchStatus.On;
+      setSwitchStatus(SoundSwitchStatus.On);
+      // console.log('pause', status);
+
+      // return status;
+    }
   };
-
-  const onSeek = (e) => {
-    const newPos =
-      (e.nativeEvent.offsetX / e.target.clientWidth) * audioRef.current.duration;
-    audioRef.current.currentTime = newPos;
-    setProgress((newPos / audioRef.current.duration) * 100);
-  };
-
-  useEffect(() => {
-    audioRef.current.addEventListener('timeupdate', updateTime);
-    audioRef.current.addEventListener('loadeddata', () =>
-      setDuration(audioRef.current.duration)
-    );
-
-    return () => {
-      audioRef.current.removeEventListener('timeupdate', updateTime);
-      audioRef.current.removeEventListener('loadeddata', () =>
-        setDuration(audioRef.current.duration)
-      );
-    };
-  }, []);
+  // console.log('status', status);
 
   return (
-    <div>
-      <audio ref={audioRef} controls>
-        <source src={'https://vk.com/mp3/bb2.mp3'} type='audio/mp3' />
-        Your browser does not support the audio element.
-      </audio>
-      <div className='progress-bar' onClick={onSeek}>
-        <div className='progress' style={{ width: `${progress}%` }}></div>
-      </div>
-      <div>Progress: {Math.floor(progress)}%</div>
-    </div>
+    <>
+      <PlayButton
+        className={cn(s.audioPlayer__btn)}
+        onClick={soundPlay}
+        switchStatus={switchStatus}
+      />
+      {/* <button onClick={() => soundPlay(audio)}>sound</button> */}
+    </>
   );
+
+  // const audioRef = useRef(null);
+  // const [progress, setProgress] = useState(0);
+  // const [duration, setDuration] = useState(0);
+
+  // const updateTime = () => {
+  //   const current = audioRef.current.currentTime;
+  //   const total = audioRef.current.duration;
+  //   setProgress((current / total) * 100);
+  // };
+
+  // const onSeek = (e) => {
+  //   const newPos =
+  //     (e.nativeEvent.offsetX / e.target.clientWidth) * audioRef.current.duration;
+  //   audioRef.current.currentTime = newPos;
+  //   setProgress((newPos / audioRef.current.duration) * 100);
+  // };
+
+  // useEffect(() => {
+  //   audioRef.current.addEventListener('timeupdate', updateTime);
+  //   audioRef.current.addEventListener('loadeddata', () =>
+  //     setDuration(audioRef.current.duration)
+  //   );
+
+  //   return () => {
+  //     audioRef.current.removeEventListener('timeupdate', updateTime);
+  //     audioRef.current.removeEventListener('loadeddata', () =>
+  //       setDuration(audioRef.current.duration)
+  //     );
+  //   };
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <audio ref={audioRef} controls>
+  //       <source src={'https://vk.com/mp3/bb2.mp3'} type='audio/mp3' />
+  //       Your browser does not support the audio element.
+  //     </audio>
+  //     <div className='progress-bar' onClick={onSeek}>
+  //       <div className='progress' style={{ width: `${progress}%` }}></div>
+  //     </div>
+  //     <div>Progress: {Math.floor(progress)}%</div>
+  //   </div>
+  // );
 };
