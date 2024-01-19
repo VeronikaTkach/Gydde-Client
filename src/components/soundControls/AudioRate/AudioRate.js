7b8091e014f98defc31c95f05f600f639cb39056
Veronika <xafling@mail.ru>
2024-01-20 00:01:05 +0200

    tune AudioRate

--- src/components/soundControls/AudioRate/AudioRate.js
+++ src/components/soundControls/AudioRate/AudioRate.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 import s from './style.module.scss';
 import { Button } from '../../ui/buttons/Button';
+import sound from '../../../assets/images/sound.svg';
 
 export function AudioRate() {
   const [rate, setRate] = useState(1);
@@ -15,8 +16,17 @@ export function AudioRate() {
   // }, []);
 
   return (
-    <div className={cn(s.audioRate)}>
-      <Button className={s.audioRate__btn} onClick={toggleRate}>{rate === 1 ? '1x' : '2x'}</Button>
-    </div>
+    <>
+      <div className={s.audioRate}>
+        <div className={s.audioRate_main}>
+          <Button className={s.audioRate__btn} onClick={toggleRate}>
+            {rate === 1 ? '1x' : '2x'}
+          </Button>
+        </div>
+        <button className={cn(s.audioRate__sound)}>
+          <img src={sound} />
+        </button>
+      </div>
+    </>
   );
 }
