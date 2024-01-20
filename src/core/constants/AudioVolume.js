523ae20737865b59decbd89a216f6e19622cecd1
Elvira <gaellru@gmail.com>
2024-01-21 02:02:24 +0300

    add rate and volume constants

---
+++ src/core/constants/AudioVolume.js
@@ -0,0 +1,6 @@
+export const AudioVolume = {
+  Off: 0,
+  On: 1,
+};
+
+Object.freeze(AudioVolume);
---
+++ src/core/constants/SoundRate.js
@@ -0,0 +1,7 @@
+export const SoundRate = {
+  Normal: 1,
+  Medium: 1.5,
+  Fast: 2,
+};
+
+Object.freeze(SoundRate);
