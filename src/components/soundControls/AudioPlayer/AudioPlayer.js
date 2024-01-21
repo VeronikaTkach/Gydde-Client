f49e533d432b0fbca7a418353d0ad0e4fdd675b0
Elvira <gaellru@gmail.com>
2024-01-21 21:54:34 +0300

    add audio buttons and players connection logic

--- src/components/soundControls/AudioPlayer/AudioPlayer.js
+++ src/components/soundControls/AudioPlayer/AudioPlayer.js
@@ -1,18 +1,24 @@
-import { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
-import s from './style.module.scss';
+import { Prev } from '../../../assets/images/js/Prev'; // TODO добавить через иконочный шрифт
+import { usePlayer } from '../../../core/hooks/player';
 import { Button } from '../../ui/buttons/Button';
-
+import { PlayButton } from '../PlayButton/PlayButton';
+import { sound } from './AudioPlayerWithProgressBar';
+import s from './style.module.scss';
 
 export function AudioPlayer({ className }) {
-
-  useEffect(() => {
-
-  }, []);
+  const { soundSwitch, switchAudio } = usePlayer(sound);
 
   return (
-    <div className={cn(s.audioRate, className)}>
-
+    <div className={cn(s.audioPlayer, className)}>
+      <Button className={cn(s.audioPlayer__prev)}>
+        <Prev />
+      </Button>
+      <PlayButton
+        className={cn(s.audioPlayer__play)}
+        onClick={switchAudio}
+        switchStatus={soundSwitch}></PlayButton>
+      <Button className={cn(s.audioPlayer__next, 'iconRewind')}></Button>
     </div>
   );
 }
