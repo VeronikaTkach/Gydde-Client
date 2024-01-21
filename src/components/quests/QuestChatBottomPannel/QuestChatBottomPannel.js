59a7a74585ebb55e96a9d89d9f1603874982a3c7
gleg0r <egor103230@gmail.com>
2024-01-21 18:14:45 +0300

    add: chatBotttomPanel

--- src/components/quests/QuestChatBottomPannel/QuestChatBottomPannel.js
+++ src/components/quests/QuestChatBottomPannel/QuestChatBottomPannel.js
@@ -1,22 +1,87 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import cn from 'classnames';
 import s from './style.module.scss';
 import { Button } from '../../ui/buttons/Button';
 
+const placeholder = 'Enter a text';
+const guideAnswers = {
+  userAnswer: false,
+  selector: true,
+  text: 'Select an option',
+  isActive: false,
+};
+
 export function QuestChatBottomPannel({ className }) {
-  useEffect(() => {}, []);
+  const [isInputNeed, setIsInputNeed] = useState(guideAnswers.userAnswer);
+  const [isButtonActive, setIsButtonActive] = useState(guideAnswers.isActive);
+  const [isTyping, setIsTyping] = useState(false);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+  const value = useRef(null);
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+    } else if (isInputNeed) {
+      if (value.current.value !== '') {
+        setIsButtonActive(true);
+      } else {
+        setIsButtonActive(false);
+      }
+    }
+  }, [isTyping, isInputNeed]);
+
+  function handleTyping(e) {
+    if (e) setIsTyping((prev) => !prev);
+  }
+
+  function sendMessageOnKeyDown(e) {
+    if (e.key === 'Enter') {
+      setIsButtonActive(false);
+      setIsInputNeed(false);
+    }
+  }
+
+  function sendMessage() {
+    setIsButtonActive(false);
+    setIsInputNeed(false);
+  }
 
   return (
     <div className={cn(s.chatBottomPannel, className)}>
       <div className={cn(s.chatBottomPannel__answer, s.chatBottomPannel__answer_general)}>
-        <input className={cn(s.chatBottomPannel__input)} />
-        <Button />
+        {isInputNeed === true ? (
+          <input
+            ref={value}
+            className={cn(s.chatBottomPannel__input)}
+            placeholder={placeholder}
+            onChange={() => handleTyping(isInputNeed)}
+            onKeyDown={(e) => sendMessageOnKeyDown(e)}
+          />
+        ) : (
+          <div
+            className={cn(
+              guideAnswers.selector === true
+                ? s.chatBottomPannel__text_gray
+                : s.chatBottomPannel__text_white
+            )}>
+            <p>{guideAnswers.text}</p>
+          </div>
+        )}
+        <Button
+          className={cn(
+            isButtonActive === true
+              ? s.chatBottomPannel__send
+              : s.chatBottomPannel__send_disabled,
+            'iconArrowSend'
+          )}
+          onClick={() => sendMessage()}
+        />
       </div>
-      <div
+      {/* <div
         className={cn(
           s.chatBottomPannel__answer,
           s.chatBottomPannel__answer_buttons
-        )}></div>
+        )}></div> */}
     </div>
   );
 }
