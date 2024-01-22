4bf07db5f3e6cddc0682bf07196d099fedd16468
Elvira <gaellru@gmail.com>
2024-01-22 19:59:08 +0300

    fix svg icon

--- src/components/soundControls/SoundVolume/SoundVolume.js
+++ src/components/soundControls/SoundVolume/SoundVolume.js
@@ -1,24 +1,24 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import cn from 'classnames';
 import s from './style.module.scss';
 import { Button } from '../../ui/buttons/Button';
-import soundOn from '../../../assets/images/sound.svg'; // TODO добавить через иконочный шрифт
-import soundOff from '../../../assets/images/soundOff.svg'; // TODO добавить через иконочный шрифт
 import { AudioVolume } from '../../../core/constants/AudioVolume';
 import { setAudioVolume } from '../../../core/store/slices/soundSettingsSlice';
 
 export function SoundVolume() {
   const [soundVolume, setSoundVolume] = useState(AudioVolume.On);
-  const [soundVolumeImg, setSoundVolumeImg] = useState(soundOn);
   const dispatch = useDispatch();
+
+  const classes = cn(s.soundVolume, {
+    [s.soundVolume__off]: soundVolume === AudioVolume.Off,
+  });
+
   const toggleVolume = () => {
     if (soundVolume) {
       setSoundVolume(AudioVolume.Off);
-      setSoundVolumeImg(soundOff);
     } else {
       setSoundVolume(AudioVolume.On);
-      setSoundVolumeImg(soundOn);
     }
   };
 
@@ -26,9 +26,5 @@ export function SoundVolume() {
     dispatch(setAudioVolume(soundVolume));
   }, [soundVolume]);
 
-  return (
-    <Button className={cn(s.soundVolume)} onClick={toggleVolume}>
-      <img src={soundVolumeImg} />
-    </Button>
-  );
+  return <Button className={classes} onClick={toggleVolume}></Button>;
 }
