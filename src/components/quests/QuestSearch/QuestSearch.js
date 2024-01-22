08b0e777df3792078c361ddb1808a02732cd23ef
Veronika <xafling@mail.ru>
2024-01-22 14:51:03 +0200

    wallet in progress

--- src/components/quests/QuestSearch/QuestSearch.js
+++ src/components/quests/QuestSearch/QuestSearch.js
@@ -1,15 +1,42 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import cn from 'classnames';
 import s from './style.module.scss';
 import { Button } from '../../ui/buttons/Button';
 import { InputSearch } from '../../ui/Input/InputSearch';
 
-export function QuestSearch({ className }) {
-  useEffect(() => {}, []);
+export function QuestSearch(numberOfCards) {
+  // useEffect(() => {}, []);
+  const [isSearchVisible, setSearchVisible] = useState(false);
+  const [isTitleVisible, setTitleVisible] = useState(true);
+
+  // const isScroll = 600 - numberOfCards * 92;
+  // const isScroll = numberOfCards;
+
+  // if (isScroll < 6) {
+  //   setTitleVisible(true);
+  //   setSearchVisible(false);
+  // } else {
+  //   setSearchVisible(true);
+  //   setTitleVisible(false);
+  // }
 
   return (
-    <div className={cn(s.questSearch, className)}>
-      <InputSearch className={s.questSearch__input}></InputSearch>
-    </div>
+    <>
+      {isSearchVisible && (
+        <div className={cn(s.questSearch)}>
+          <InputSearch className={s.questSearch__input}/>
+          <Button
+            className={cn(s.field__btn, s.field__btn__search, 'iconSearch')}
+            type={'button'}
+            // onClick={searchHandler}
+          />
+        </div>
+      )}
+      {isTitleVisible && (
+        <div className={s.questSearch__title}>
+          <InputSearch placeholder={'My guides'} disabled={true} />
+        </div>
+      )}
+    </>
   );
 }
