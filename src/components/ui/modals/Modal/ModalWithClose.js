c88308f73d7ae07af98c3e40283c378ddff19b05
Elvira <gaellru@gmail.com>
2024-01-25 03:11:19 +0300

    add styles settings for modals

--- src/components/ui/modals/Modal/ModalWithClose.js
+++ src/components/ui/modals/Modal/ModalWithClose.js
@@ -2,11 +2,14 @@ import cn from 'classnames';
 import s from './style.module.scss';
 import { Button } from '../../buttons/Button';
 
-const ModalWithClose = ({ Component, children, className, onClose }) => {
+const ModalWithClose = ({ Component, children, className, onClose, styles }) => {
   return (
-    <Component className={className}>
+    <Component className={className} styles={styles}>
       <div className={s.modal__header}>
-        <Button className={cn(s.modal__close, 'iconClose')} onClick={onClose}></Button>
+        <Button
+          className={cn(s.modal__close, 'iconClose')}
+          onClick={onClose}
+          style={{ top: styles?.top }}></Button>
       </div>
       <div>{children}</div>
     </Component>
