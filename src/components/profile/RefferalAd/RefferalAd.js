24172acddf0f3f226214a560bf834bceb496045c
Veronika <xafling@mail.ru>
2024-01-28 22:35:49 +0200

    tune ReferralAd

--- src/components/profile/RefferalAd/RefferalAd.js
+++ src/components/profile/RefferalAd/RefferalAd.js
@@ -14,52 +14,52 @@ export function ReferralAd({ className, staticTextProfileReferrals }) {
   // useEffect(() => {}, []);
 
   return (
-    <div className={cn(s.Referral, className)}>
-      <div className={s.Referral__adTitle}>
-        <span className={s.Referral__textTitle}>
+    <div className={cn(s.referral, className)}>
+      <div className={s.referral__adTitle}>
+        <span className={s.referral__textTitle}>
           {staticTextProfileReferrals?.adTitle ||
             STATIC_TEXT[PageName.ProfileReferrals].adTitle}
         </span>
       </div>
-      <div className={s.Referral__field}>
-        <img className={s.Referral__img} src={mascotFriend} alt={'mascot with friend'} />
-        <div className={s.Referral__info}>
-          <div className={s.Referral__container}>
+      <div className={s.referral__field}>
+        <img className={s.referral__img} src={mascotFriend} alt={'mascot with friend'} />
+        <div className={s.referral__info}>
+          <div className={s.referral__container}>
             <div>
               <img
-                className={cn(s.Referral__icon, s.Referral__icon_geo)}
+                className={cn(s.referral__icon, s.referral__icon_geo)}
                 src={geo}
                 alt={'geo'}
               />
             </div>
-            <span className={s.Referral__text}>
+            <span className={s.referral__text}>
               {staticTextProfileReferrals?.rewardForCompletion ||
                 STATIC_TEXT[PageName.ProfileReferrals].rewardForCompletion}
             </span>
-            <span className={s.Referral__text}>~</span>
-            <span className={s.Referral__cost}>$3</span>
+            <span className={s.referral__text}>~</span>
+            <span className={s.referral__cost}>$3</span>
           </div>
-          <div className={s.Referral__container}>
+          <div className={s.referral__container}>
             <div>
-              <img className={s.Referral__icon_frame} src={friend} alt={'friend'} />
+              <img className={s.referral__icon_frame} src={friend} alt={'friend'} />
             </div>
-            <span className={s.Referral__text}>
+            <span className={s.referral__text}>
               {staticTextProfileReferrals?.referralReward ||
                 STATIC_TEXT[PageName.ProfileReferrals].referralReward}
             </span>
-            <span className={s.Referral__text}>~</span>
-            <span className={s.Referral__cost}>$1.5</span>
+            <span className={s.referral__text}>~</span>
+            <span className={s.referral__cost}>$1.5</span>
           </div>
-          <div className={s.Referral__container}>
+          <div className={s.referral__container}>
             <div>
-              <img className={s.Referral__icon} src={people} alt={'people'} />
+              <img className={s.referral__icon} src={people} alt={'people'} />
             </div>
-            <span className={s.Referral__text}>
+            <span className={s.referral__text}>
               {staticTextProfileReferrals?.friendReward ||
                 STATIC_TEXT[PageName.ProfileReferrals].friendReward}
             </span>
-            <span className={s.Referral__text}>~</span>
-            <span className={s.Referral__cost}>$0.75</span>
+            <span className={s.referral__text}>~</span>
+            <span className={s.referral__cost}>$0.75</span>
           </div>
         </div>
       </div>
