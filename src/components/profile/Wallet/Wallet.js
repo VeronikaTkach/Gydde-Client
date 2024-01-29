b378946bd85ffb49db9d43eb5941ae8c7fb0cfbd
gleg0r <egor103230@gmail.com>
2024-01-29 15:52:11 +0300

    change: img position, nubmer, locig for copy

--- src/components/profile/Wallet/Wallet.js
+++ src/components/profile/Wallet/Wallet.js
@@ -8,7 +8,18 @@ import iconEthereum from '../../../assets/images/Ethereum.svg';
 import { ConnectWalletStatus } from '../../../core/constants/Status';
 import { NetworkName } from '../../../core/constants/NetworkName';
 
+const number = '0x7824b81';
+const firstItem = 0;
+const fourthItem = 4;
+
 export function Wallet({ className, staticText }) {
+  const newNumberPart1 = number.substring(firstItem, fourthItem);
+  const newNumberPart2 = number.substring(number.length - fourthItem, number.length);
+  const newNumber = newNumberPart1 + '...' + newNumberPart2;
+  function copyOnClick() {
+    navigator.clipboard.writeText(number);
+  }
+
   return (
     <>
       <div className={cn(s.wallet, className)}>
@@ -19,8 +30,8 @@ export function Wallet({ className, staticText }) {
           </div>
           <div className={s.wallet__field_info}>
             <div className={cn(s.wallet__field_info_number)}>
-              <div>number</div>
-              <button className={cn(s.wallet__icon, 'iconCopy')} />
+              <div>{newNumber}</div>
+              <button className={cn(s.wallet__icon, 'iconCopy')} onClick={copyOnClick} />
               <button className={cn(s.wallet__icon, 'iconLink')} />
             </div>
             <div className={cn(s.wallet__field_info_status)}>
